77773ef2c7ee3315735ae6263037a81040bb39ba
UwUlesson <nuevocubi@gmail.com>
2025-02-25 22:01:25 -0500

    algunas lineas de codigo

--- getData.py
+++ getData.py
@@ -1,3 +1,6 @@
 from dotenv import load_dotenv
+import os
 import requests
+load_dotenv("ClientID.env")
 response = requests.get("https://api.myanimelist.net/v2")
+client_id = os.getenv("CLIENT_ID")
